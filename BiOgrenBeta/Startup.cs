e0d4317b2f5b81d05591a9acbd0a333e4654af9e
Emre <ukabemre@gmail.com>
2021-05-23 17:20:03 +0300

    Pagination

--- BiOgrenBeta/Startup.cs
+++ BiOgrenBeta/Startup.cs
@@ -6,6 +6,7 @@ using BiOgrenBeta.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,10 @@ namespace BiOgrenBeta
 
             services.AddDbContext<Context>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.AddMvc();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
